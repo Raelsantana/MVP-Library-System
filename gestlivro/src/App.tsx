a9ad415bf22e3e0f216e494fb21ac1c266bbd595
Raelsantana <raelsantana1@hotmail.com>
2025-04-10 22:16:31 -0300

    Adicionando a página de cadastro de livros(sem backend)

--- gestlivro/src/App.tsx
+++ gestlivro/src/App.tsx
@@ -1,10 +1,20 @@
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css'
 import AcervoPage from './pages/Acervo'
+import RegisterBookPage from './pages/RegitroLivros/';
 
 function App() {
   return (
-    <AcervoPage />
+    <Router>
+      
+      <Routes>
+    <Route path="/" element={<AcervoPage />} />
+
+    <Route path="/register-book" element={<RegisterBookPage />} />
+    </Routes>
+
+    </Router>
+
   )
 }
 
