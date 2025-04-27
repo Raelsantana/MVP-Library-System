c38bad2dc48c16c3937e383d369711171b910e4c
Tiago Oliveira - TiagoDev0708 <tiagoj0708@gmail.com>
2025-04-27 03:57:28 -0300

    feat: Add: Adicionando conexão de exclusão de usuario

--- gestlivro/src/services/usersservice.ts
+++ gestlivro/src/services/usersservice.ts
@@ -1,12 +1,18 @@
 import axios from "axios";
 import { API_URL } from "../utils/api";
 
-export const cadastrarUsuario = async (data: string) => {
+export const cadastrarUsuario = async (data: any) => {
     const response = await axios.post(`${API_URL}users/adicionar`, data);
     return response.data;
 };
+
 
 export const listarUsuarios = async () => {
     const response = await axios.get(`${API_URL}users/listar`);
     return response.data;
 };
+
+export const removerUsuario = async (id?: number) => {
+    const response = await axios.delete(`${API_URL}users/deletar/${id}`);
+    return response.data;
+  }
